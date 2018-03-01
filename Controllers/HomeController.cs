854f5aff7ee4852c8c9fc82f342b56a0c7b9bd75
Hamad-Khalid <32792523+Hamad-Khalid@users.noreply.github.com>
2018-03-01 19:53:17 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMedia.Core.Models;
+using WebMedia.DAL.DataContext;
+    namespace WebMedia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Product product = new Product("Chess", "Board Game", 10);
+            ProductDC pdc = new ProductDC();
+            pdc.AddProduct(product);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}

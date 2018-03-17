c7260dc60ecb0941686346b375625e5474426a66
Hamad-Khalid <32792523+Hamad-Khalid@users.noreply.github.com>
2018-03-17 18:12:39 +0300

    29-06-1439: Commit
    Finished Create, Delete, Update and List Products

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -17,16 +17,45 @@ namespace WebMedia.Controllers
             List<Product> products = pdc.ProductList();
             return View(products);
         }
+
         public ActionResult AddProductPage()
         {
             return View();
         }
+
         public ActionResult AddProduct(Product product)
         {
-            Product productToCreate = new Product(product.Name,product.Category,product.Price);
-            string test = product.Id;
+            Product productToCreate = new Product(product.Name, product.Category, product.Price);
             pdc.AddProduct(productToCreate);
             return RedirectToAction("ProductList");
+        }
+        public ActionResult DeleteProductPage(int Height, string id, string message = "")
+        {
+            Product productToDelete = pdc.FindProductById(id);
+            ViewBag.message = message;
+            return View(productToDelete);
+        }
+
+        public ActionResult DeleteProduct(string Id)
+        {
+            if (pdc.DeleteProduct(Id)) return RedirectToAction("ProductList");
+            else
+            {
+                return RedirectToAction("DeleteProductPage", new { Height =20, id = Id, message = "Not Found" });
+            }
+
+        }
+
+        public ActionResult UpdateProductPage(string id)
+        {
+            Product product = pdc.FindProductById(id);
+            return View(product);
+        }
+
+        public ActionResult UpdateProduct(Product product) 
+        {
+            pdc.UpdateProduct(product);
+            return RedirectToAction("UpdateProductPage",new { id = product.Id});
         }
     }
 }
